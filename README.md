d0ec10d7e099f42743c0d23fb5641713641718f5
LevAntipov <levantipov@outlook.com>
2025-11-22 23:37:09 +0300

    html -> js

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# OzonBank
+Компонент прогресса
+const component = new ProgressComponent(container,options)
+container - место, куда будет помещён компонент
+options - {
+    initialValue:string | number,
+    isAnimated:boolean,
+    isHiden:boolean
+}
+.hide() - скрыть
+.show() - показать
+.run() - начать анимацию
+.stop() - остановить анимацию
+.setValue() - установить значение прогресса
--- oz.js
+++ oz.js
@@ -1,58 +1,191 @@
-function progressComponentApi(
-    initialValueState,
-    rotationSpeed,
-    isAnimated
-) {
-    const progressCircle = document.querySelector('.progressCircle');
-    const valueInput = document.querySelector('.progressInput');
-    const toggles = document.querySelectorAll('.toggle');
-    const toggleCircle = document.querySelectorAll('.toggleCircle');
+class ProgressComponent {
+    constructor(container, options = {
+        isAnimated:false,
+        initialValue:15,
+        isHiden:false
+    }) {
+        this._container = container
+        this.isAnimated = options.isAnimated
+        this.value = options.initialValue
+        this.isHiden = options.isHiden
 
-    valueInput.value = initialValueState
+        this._render()
+        this._addEvents()
 
-    function setValue(v) {
-        const deg = (v / 100) * 360;
-        progressCircle.style.background = `conic-gradient(#3f7bff 0deg ${deg}deg, rgba(223, 234, 245, 1) ${deg}deg 360deg)`;
+        this.setValue(this.value)
     }
 
-    toggles[0].addEventListener('click', () => {
-        toggles[0].classList.toggle('on');
+    setValue(v) {
+        const deg = (v / 100) * 360;
+        this._progressCircle.style.background = `conic-gradient(#3f7bff 0deg ${deg}deg, rgba(223, 234, 245, 1) ${deg}deg 360deg)`;
+    }
 
-        if (toggles[0].classList.contains('on')) {
-            toggleCircle[0].style.translate = '100%'
-            progressCircle.style.animation = `spin ${rotationSpeed}s infinite linear`;
+    _toggleAnimateFunc() {
+        if (this.isAnimated) {
+            this._toggleAnimate.style.backgroundColor = 'rgb(63, 123, 255)'
+            this._toggleCircleAnimate.style.translate = '100%'
+            this._progressCircle.style.animation = `spin 3s infinite linear`
         } else {
-            toggleCircle[0].style.translate = ''
-            progressCircle.style.animation = 'none';
+            this._toggleCircleAnimate.style.translate = ''
+            this._progressCircle.style.animation = 'none';
+            this._toggleAnimate.style.backgroundColor = 'rgba(223, 234, 245, 1)'
         }
-    });
-    toggles[1].addEventListener('click', () => {
-        toggles[1].classList.toggle('on');
-        if (toggles[1].classList.contains('on')) {
-            toggleCircle[1].style.translate = '100%'
+    }
+
+    _toggleHideFunc() {
+        if(this.isHiden){
+            this._toggleCircleHide.style.translate = '100%'
+            this._toggleHide.style.backgroundColor = 'rgb(63, 123, 255)'
+            this._progressCircle.style.display = 'none'
         }
-        else {
-            toggleCircle[1].style.translate = ''
+        else{
+            this._toggleCircleHide.style.translate = ''
+            this._toggleHide.style.backgroundColor = 'rgba(223, 234, 245, 1)'
+            this._progressCircle.style.display = 'flex'
         }
-        progressCircle.style.display = toggles[1].classList.contains('on') ? 'none' : 'flex';
-    });
+    }
 
-    valueInput.addEventListener('input', () => setValue(valueInput.value));
+    _addEvents() {
+        this._toggleAnimateFunc()
 
-    setValue(valueInput.value);
+        this._toggleHideFunc()
 
-    if (isAnimated) {
-        const clickEvent = new MouseEvent('click', {
-            bubbles: true,
-            cancelable: true
+        this._toggleAnimate.addEventListener('click', () => {
+            this.isAnimated = !this.isAnimated
+            this._toggleAnimateFunc()
+        })
+
+        this._toggleHide.addEventListener('click', () => {
+            this.isHiden = !this.isHiden
+            this._toggleHideFunc()
         });
 
-        toggles[0].dispatchEvent(clickEvent)
+        this._valueInput.addEventListener('input', () => this.setValue(this._valueInput.value));
+    }
+
+    _render() {
+        const divApp = document.createElement('div')
+        divApp.classList.add('app')
+        this._container.appendChild(divApp)
+
+        const divTitle = document.createElement('div')
+        divTitle.classList.add('title')
+        divTitle.innerHTML = 'Progress'
+        divApp.appendChild(divTitle)
+
+        //DIV progress 
+        const divProgress = document.createElement('div')
+        divProgress.classList.add('progress')
+        divApp.appendChild(divProgress)
+
+        const divProgressCircle = document.createElement('div')
+        divProgressCircle.classList.add('progressCircle')
+        divProgress.appendChild(divProgressCircle)
+
+        const divCenterCircle = document.createElement('div')
+        divCenterCircle.classList.add('centerCircle')
+        divProgressCircle.appendChild(divCenterCircle)
+        //DIV progress 
+
+
+        //DIV controls
+        const divControls = document.createElement('div')
+        divControls.classList.add('controls')
+        divApp.appendChild(divControls)
+
+        //....DIV valueControl
+        const divValueControl = document.createElement('div')
+        divValueControl.classList.add('valueControl')
+        divControls.appendChild(divValueControl)
+
+        const divBuffer = document.createElement('div')
+        divBuffer.classList.add('buffer')
+        divValueControl.appendChild(divBuffer)
+
+        const inputProgressInput = document.createElement('input')
+        inputProgressInput.classList.add('progressInput')
+        inputProgressInput.value = this.value
+        divBuffer.appendChild(inputProgressInput)
+
+
+        const spanValue = document.createElement('span')
+        spanValue.innerHTML = 'Value'
+        divValueControl.appendChild(spanValue)
+        //....DIV valueControl
+
+        //....DIV animateToggle
+        const divAnimateToggle = document.createElement('div')
+        divAnimateToggle.classList.add('animateToggle')
+        divControls.appendChild(divAnimateToggle)
+
+        const divBuffer2 = document.createElement('div')
+        divBuffer2.classList.add('buffer')
+        divAnimateToggle.appendChild(divBuffer2)
+
+        const divToggle1 = document.createElement('div')
+        divToggle1.classList.add('toggle')
+        divBuffer2.appendChild(divToggle1)
+
+        const divToggleCircle = document.createElement('div')
+        divToggleCircle.classList.add('toggleCircle')
+        divToggle1.appendChild(divToggleCircle)
+
+
+        const spanValue2 = document.createElement('span')
+        spanValue2.innerHTML = 'Animate'
+        divAnimateToggle.appendChild(spanValue2)
+        //....DIV animateToggle
+
+        //....DIV hideToggle
+        const divHideToggle = document.createElement('div')
+        divHideToggle.classList.add('hideToggle')
+        divControls.appendChild(divHideToggle)
+
+        const divBuffer3 = document.createElement('div')
+        divBuffer3.classList.add('buffer')
+        divHideToggle.appendChild(divBuffer3)
+
+        const divToggle2 = document.createElement('div')
+        divToggle2.classList.add('toggle')
+        divBuffer3.appendChild(divToggle2)
+
+        const divToggleCircle2 = document.createElement('div')
+        divToggleCircle2.classList.add('toggleCircle')
+        divToggle2.appendChild(divToggleCircle2)
+
+
+        const spanValue3 = document.createElement('span')
+        spanValue3.innerHTML = 'Hide'
+        divHideToggle.appendChild(spanValue3)
+        //....DIV hideToggle
+
+        this._toggleAnimate = divToggle1
+        this._toggleCircleAnimate = divToggleCircle
+
+        this._toggleHide = divToggle2
+        this._toggleCircleHide = divToggleCircle2
+
+        this._progressCircle = divProgressCircle
+
+        this._valueInput = inputProgressInput
+    }
+
+    hide(){
+        this.isHiden = true
+        this._toggleHideFunc()
+    }
+    show(){
+        this.isHiden = false
+        this._toggleHideFunc()
+    }
+    run(){
+        this.isAnimated = true
+        this._toggleAnimateFunc()
+    }
+    stop(){
+        this.isAnimated = false
+        this._toggleAnimateFunc()
     }
 }
 
-progressComponentApi(
-    50,
-    10,
-    true
-)
+let progr = new ProgressComponent(document.body, { initialValue: 30, isAnimated: false, isHiden: false })
