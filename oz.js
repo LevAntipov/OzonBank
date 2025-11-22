8e5587bf8d403d97dcfce3d44f90f2bf902ff1b1
LevAntipov <levantipov@outlook.com>
2025-11-22 15:10:16 +0300

    fixed bugs

--- oz.js
+++ oz.js
@@ -1,48 +1,58 @@
-const progressCircle = document.querySelector('.progress-circle');
-const valueInput = document.querySelector('input');
-const toggles = document.querySelectorAll('.toggle');
-const toggleCircle = document.querySelectorAll('.toggleCircle');
+function progressComponentApi(
+    initialValueState,
+    rotationSpeed,
+    isAnimated
+) {
+    const progressCircle = document.querySelector('.progressCircle');
+    const valueInput = document.querySelector('.progressInput');
+    const toggles = document.querySelectorAll('.toggle');
+    const toggleCircle = document.querySelectorAll('.toggleCircle');
 
-function setValue(v) {
-    const val = Math.max(0, Math.min(100, v));
-    const deg = (val / 100) * 360;
-    progressCircle.style.background = `conic-gradient(#3f7bff 0deg ${deg}deg, #dfe6ee ${deg}deg 360deg)`;
+    valueInput.value = initialValueState
+
+    function setValue(v) {
+        const deg = (v / 100) * 360;
+        progressCircle.style.background = `conic-gradient(#3f7bff 0deg ${deg}deg, rgba(223, 234, 245, 1) ${deg}deg 360deg)`;
+    }
+
+    toggles[0].addEventListener('click', () => {
+        toggles[0].classList.toggle('on');
+
+        if (toggles[0].classList.contains('on')) {
+            toggleCircle[0].style.translate = '100%'
+            progressCircle.style.animation = `spin ${rotationSpeed}s infinite linear`;
+        } else {
+            toggleCircle[0].style.translate = ''
+            progressCircle.style.animation = 'none';
+        }
+    });
+    toggles[1].addEventListener('click', () => {
+        toggles[1].classList.toggle('on');
+        if (toggles[1].classList.contains('on')) {
+            toggleCircle[1].style.translate = '100%'
+        }
+        else {
+            toggleCircle[1].style.translate = ''
+        }
+        progressCircle.style.display = toggles[1].classList.contains('on') ? 'none' : 'flex';
+    });
+
+    valueInput.addEventListener('input', () => setValue(valueInput.value));
+
+    setValue(valueInput.value);
+
+    if (isAnimated) {
+        const clickEvent = new MouseEvent('click', {
+            bubbles: true,
+            cancelable: true
+        });
+
+        toggles[0].dispatchEvent(clickEvent)
+    }
 }
 
-
-function toggleSwitch(el, state) {
-    debugger
-    if (state) el.classList.add('on');
-    else el.classList.remove('on');
-}
-
-
-toggles[0].addEventListener('click', () => {
-    toggles[0].classList.toggle('on');
-
-    if (toggles[0].classList.contains('on')) {
-        toggleCircle[0].style.translate = '100%'
-        progressCircle.style.animation = 'spin 3s infinite linear';
-    } else {
-        toggleCircle[0].style.translate = ''
-        progressCircle.style.animation = 'none';
-    }
-});
-
-
-toggles[1].addEventListener('click', () => {
-    toggles[1].classList.toggle('on');
-    if(toggles[1].classList.contains('on')){
-        toggleCircle[1].style.translate = '100%'
-    }
-    else{
-        toggleCircle[1].style.translate = ''
-    }
-    progressCircle.style.display = toggles[1].classList.contains('on') ? 'none' : 'flex';
-});
-
-
-valueInput.addEventListener('input', () => setValue(valueInput.value));
-
-
-setValue(valueInput.value);
+progressComponentApi(
+    50,
+    10,
+    true
+)
